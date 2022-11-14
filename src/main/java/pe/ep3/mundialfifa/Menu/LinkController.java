c653c92177087c67fdf233e6ac33d9fc862addb1
FlavioLecca <fla.lecca31@gmail.com>
2022-11-13 23:14:42 -0500

    add jugador

---
+++ src/main/java/pe/ep3/mundialfifa/Menu/LinkController.java
@@ -0,0 +1,23 @@
+package pe.ep3.mundialfifa.Menu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class LinkController {
+
+    @GetMapping
+    @RequestMapping("/mundial")
+    public List<LinkResponse> getLink(){
+        List<LinkResponse> linkResponses = List.of(LinkResponse.builder().link("https://app-mundial-fifa.herokuapp.com/api/v1/mundial/paises").build(),
+                LinkResponse.builder().link("https://app-mundial-fifa.herokuapp.com/api/v1/mundial/jugadores").build(),
+                LinkResponse.builder().link("https://app-mundial-fifa.herokuapp.com/api/v1/mundial/estadios").build(),
+                LinkResponse.builder().link("https://app-mundial-fifa.herokuapp.com/api/v1/mundial/add").build());
+        return linkResponses;
+    }
+}
---
+++ src/main/java/pe/ep3/mundialfifa/Menu/LinkResponse.java
@@ -0,0 +1,10 @@
+package pe.ep3.mundialfifa.Menu;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LinkResponse {
+    private String link;
+}

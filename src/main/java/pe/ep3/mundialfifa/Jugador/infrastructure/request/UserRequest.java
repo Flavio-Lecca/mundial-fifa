e563bc9eaf9e63ccf7f5d3e0de637db30be2637f
FlavioLecca <fla.lecca31@gmail.com>
2022-11-13 23:46:59 -0500

    add jugador

--- src/main/java/pe/ep3/mundialfifa/Jugador/infrastructure/request/UserRequest.java
+++ src/main/java/pe/ep3/mundialfifa/Jugador/infrastructure/request/UserRequest.java
@@ -1,9 +1,13 @@
 package pe.ep3.mundialfifa.Jugador.infrastructure.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class UserRequest {
     private Long id;

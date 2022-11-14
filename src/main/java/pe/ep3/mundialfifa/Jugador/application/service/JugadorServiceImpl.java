2c561c1bc3771f4cf1cb7627e376123331abb2fb
FlavioLecca <fla.lecca31@gmail.com>
2022-11-13 22:50:42 -0500

    add jugador

--- src/main/java/pe/ep3/mundialfifa/Jugador/application/service/JugadorServiceImpl.java
+++ src/main/java/pe/ep3/mundialfifa/Jugador/application/service/JugadorServiceImpl.java
@@ -48,6 +48,7 @@ public class JugadorServiceImpl implements JugadorService{
 
     public Jugador buildPersonFromRequest(UserRequest request) {
         return Jugador.builder()
+                .id(request.getId())
                 .jugador(request.getJugador())
                 .pais(request.getPais())
                 .edad(request.getEdad())
